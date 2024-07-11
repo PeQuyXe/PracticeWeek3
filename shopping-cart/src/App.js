daba5604231fc31531d4ddf437860d20ce3af44a
PeQuyXe <cuongpham17072002@gmail.com>
2024-07-11 08:45:59 +0700

    fix warm

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
       <div className="App">
         <Navbar />
       </div>
-      <div clasName="body">
+      <div className="body">
         <Outlet></Outlet>
       </div>
     </div>
